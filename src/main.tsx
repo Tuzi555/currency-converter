7a45bf0bf5f1a1711a778c1c9f7ff247de4c884b
tuzi555 <jakub.tuzar@gmail.com>
2024-10-04 00:54:23 +0200

    add tests for currency converter

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,13 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorPage } from './components/error-page.tsx';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: process.env.NODE_ENV === 'test' ? false : 3,
+        },
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
--- tests/e2e/currency-converter.spec.ts
+++ tests/e2e/currency-converter.spec.ts
@@ -8,7 +8,7 @@ Country|Currency|Amount|Code|Rate
 Bulgaria|lev|1|BGN|12.941
 Brazil|real|1|BRL|4.219
 Canada|dollar|1|CAD|16.955
-Australia|dollar|1|AUD|15.772
+Australia|dollar|1|AUD|10
 `;
             await route.fulfill({ body });
         });
@@ -21,21 +21,36 @@ Australia|dollar|1|AUD|15.772
 
     test('has exchange rate table', async ({ page }) => {
         const tableRows = page.getByTestId('exchange-rate-row');
-
         await expect(tableRows).toHaveCount(4);
     });
 
     test('has currency converter', async ({ page }) => {
-        const tableRows = page.getByTestId('currency-converter');
+        const currencyConverter = page.getByTestId('currency-converter');
+        await expect(currencyConverter).toBeVisible();
+    });
 
-        expect(tableRows).toBeDefined();
+    test('cannot submit without the conversion amount', async ({ page }) => {
+        const promptText = 'Fill in the form to see the results.';
+        await expect(page.getByText(promptText)).toBeVisible();
+        await page.getByRole('button').click();
+        await expect(page.getByText(promptText)).toBeVisible();
+    });
+
+    test('successful submission', async ({ page }) => {
+        const promptText = 'Fill in the form to see the results.';
+        await expect(page.getByText(promptText)).toBeVisible();
+        await page.getByLabel('CZK amount').fill('100');
+        await page.getByRole('button').click();
+        await expect(page.getByText('10 AUD')).toBeVisible();
     });
 
     test('shows error page on fetch fail', async ({ page }) => {
         await page.route('*/**/cnb-exchange-rates', async (route) => {
             await route.fulfill({ status: 500 });
         });
-
-        expect(page.getByText(/Oops.... something went wrong./)).toBeDefined();
+        await page.reload();
+        await expect(
+            page.getByText(/Oops.... something went wrong./)
+        ).toBeVisible();
     });
 });
